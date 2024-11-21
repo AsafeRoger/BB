b8f5a0f68698f6ceffaf42feb59fc696303bcb72
Asafe Roger <134728635+AsafeRoger@users.noreply.github.com>
2024-11-21 14:34:45 -0300

    Update ContaCorrente.php

--- Modelos/ContaCorrente.php
+++ Modelos/ContaCorrente.php
@@ -5,6 +5,9 @@ require_once("IPix.php");
 
 class ContaCorrente extends Conta implements IPix {
 
+    private $destinatariosPix = [];
+
+
     public function Pix($valor, $contaDestino) {
         if ($valor <= 0) {
             echo "Valor inválido para o Pix.\n";
@@ -12,13 +15,23 @@ class ContaCorrente extends Conta implements IPix {
         }
 
         if ($this->saldo >= $valor) {
-            // Deduz do saldo da conta de origem
+            //set q o saldo vai ser saldo menos valor do pix
             $this->setSaldo($this->saldo - $valor);
-            // Adiciona o valor à conta destino
+            //adiciona o saldo a conta destino 
             $contaDestino->setSaldo($contaDestino->getSaldo() + $valor);
             echo "Pix de R$ $valor realizado com sucesso para {$contaDestino->getNome()}.\n";
+            $this->destinatariosPix[] = $contaDestino->getNome();
+        
         } else {
             echo "Saldo insuficiente.\n";
         }
+    }
+
+    /**
+     * Get the value of destinatariosPix
+     */
+    public function getDestinatariosPix()
+    {
+        return $this->destinatariosPix;
     }
 }
